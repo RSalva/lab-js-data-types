c5a6224b1ef96618ab23a381f36f259f86e3b51f
Ramon Salva Sanchez <ramonsalvasanchez@gmail.com>
2025-03-27 21:13:50 +0100

    Se realiza el lab con las instrucciones aportadas

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,10 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let concatenatedParts = part1.charAt(0).toUpperCase() + part1.charAt(1) + part1.charAt(2) + part1.charAt(3) + part2.charAt(0).toUpperCase() + part2.charAt(1) + part2.charAt(2) + part2.charAt(3) + part2.charAt(4) + part2.charAt(5);
 
 // Print the cameLtaiL-formatted string
-
+console.log(concatenatedParts);
 
 
 
@@ -35,10 +35,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = 84 * 15 / 100;
 
 // Print out the tipAmount
-
+console.log(tipAmount);
 
 
 
@@ -47,10 +47,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const minCeiled = Math.ceil(1);
+const maxFloored = Math.floor(10);
+let randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -61,16 +63,25 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b;         // false
 
-const expression2 = a || b;
+const expression2 = a || b;         //true
 
-const expression3 = !a && b;
+const expression3 = !a && b;        //false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b);      //true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b;       //true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b);      //false
 
-const expression7 = a && a;
+const expression7 = a && a;         //true
+
+
+console.log(`La expresion 1 es : ${expression1}`);
+console.log(`La expresion 2 es : ${expression2}`);
+console.log(`La expresion 3 es : ${expression3}`);
+console.log(`La expresion 4 es : ${expression4}`);
+console.log(`La expresion 5 es : ${expression5}`);
+console.log(`La expresion 6 es : ${expression6}`);
+console.log(`La expresion 7 es : ${expression7}`);
